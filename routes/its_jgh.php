a746580b10abc595e87f973a9e4877514cebd669
Soufian Jghalef <icts21-23.336@A4-24.local>
2022-05-30 11:34:35 +0200

    Added Saluta function

--- routes/its_jgh.php
+++ routes/its_jgh.php
@@ -1,2 +1,7 @@
 <?php
     $nome = 'Soufian';
+    use Illuminate\Support\Facades\Route;
+
+    Route::get('/salve', function(Request $request){
+        return response("<h1>Ciao ".$nome."!</h1>");
+    });
